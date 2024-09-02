afedc6688cef373d48a2108c64415b4ee4df05dc
Saad Hamid <sheikhsaadhamid@hotmail.com>
2024-09-02 22:30:38 +1200

    Added Day 5 lecture and assesment

---
+++ JavaScript/Lecture5/Assessment.js
@@ -0,0 +1,5 @@
+let totalMarks = prompt("Enter Total Marks: ");
+let marksObtained = prompt("Enter marks obtained: ");
+let result = (marksObtained / totalMarks)* 100;
+document.write("Result : "+result+"%");
+alert("Result : "+result+"%");
---
+++ JavaScript/Lecture5/index.js
@@ -0,0 +1,6 @@
+alert("Hi")
+let userName = prompt("Enter your name");
+alert(userName);
+document.write("Hello "+userName);
+
+
