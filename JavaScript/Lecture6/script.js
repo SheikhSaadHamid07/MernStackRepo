2b30a4f693bf9d6282b6db2eb9c9fba04cd196aa
Saad Hamid <sheikhsaadhamid@hotmail.com>
2024-09-08 09:40:42 +1200

    Added Lecture 6

---
+++ JavaScript/Lecture6/script.js
@@ -0,0 +1,21 @@
+let a = prompt("1st");
+let b = +prompt("2nd");
+
+
+// let, var and const variables
+
+
+
+//functions 
+
+function abc(){
+    let totalMarks = prompt("Enter Total Marks: ");
+let marksObtained = prompt("Enter marks obtained: ");
+let result = (marksObtained / totalMarks)* 100;
+console.log("Result : "+result+"%");
+alert("Result : "+result+"%");
+}
+
+abc();
+abc();
+abc();
