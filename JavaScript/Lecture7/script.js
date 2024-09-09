f63bb52756da6c4e83d63d71cb4b9f548b767663
Saad Hamid <sheikhsaadhamid@hotmail.com>
2024-09-09 22:15:22 +1200

    Added Lecture 7

---
+++ JavaScript/Lecture7/script.js
@@ -0,0 +1,9 @@
+
+
+let getPercent = ()=>{
+    let totalMarks = document.getElementById("tm1").value;
+    let marksObtained = document.getElementById("mo1").value;
+    let result = (marksObtained / totalMarks) * 100;
+    document.getElementById("change1").innerHTML=(`You have got ${result}% `);
+    console.log(`You have got ${result}%`);
+}
